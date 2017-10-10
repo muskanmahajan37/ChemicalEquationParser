fc8d50a9c280c0f9b704af367598fcc3ce2fd5cf
Vinit Shahdeo <vinitshahdeo@gmail.com>
2017-10-10 23:09:37 +0530

    javascript

---
+++ js/default.js
@@ -0,0 +1,40 @@
+﻿
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                
+            } else {
+
+            }
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        
+    };
+
+    app.start();
+})();
+
+var justFinishRunningChemicalEq = false;
+
+function TypeOnElement(click_id) {
+   // window.location.reload("./element/"+click_id+".htm");
+}
+
+function typeOnConsole() {
+    justFinishRunningChemicalEq = false;
+}
+function cleanConsole() {
+    if(justFinishRunningChemicalEq==true)
+        document.getElementById("inputFormula").innerHTML = "";
+}
